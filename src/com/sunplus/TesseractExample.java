df49492bd7c7dbafb7302815ee78091bb7a66815
Yogeendra R Rao <yogeendra.r@gmail.com>
2015-09-02 14:46:19 +0530

    Initial commit

---
+++ src/com/sunplus/TesseractExample.java
@@ -0,0 +1,33 @@
+package com.sunplus;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class TesseractExample {
+
+	public static void main(String[] args) {
+		//File imageFile = new File("images/55.jpg");
+        Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
+         //Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+        	instance.setLanguage("eng");
+        	//String result = instance.doOCR(imageFile);
+        	List<ITesseract.RenderedFormat> l = new ArrayList<ITesseract.RenderedFormat>();
+        	l.add(ITesseract.RenderedFormat.TEXT);
+        	instance.createDocuments("images/55.jpg","output/log.out",l);
+        	
+            //String result = instance.doOCR(imageFile);
+           // System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());	
+        }
+
+	}
+
+}
